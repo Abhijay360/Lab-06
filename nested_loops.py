13e5bfdf9dd3877cc26f9dc098c15650001c00c8
Abhijay <73731755+Abhijay360@users.noreply.github.com>
2025-10-17 14:54:36 -0400

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -1,6 +1,6 @@
 def get_names(first_names, last_names):
-    # Combinations of the one first name with all last names
-    return [first_names[0] + " " + last_name for last_name in last_names]
+    # Use only the first first name with all last names
+    return [f"{first_names[0]} {last}" for last in last_names]
 
 def average_scores(scores):
     penalties = {0: 1.0, 1: 0.9, 2: 0.75, 3: 0.5}
